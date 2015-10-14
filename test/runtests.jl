325314128c89e317b7ade5a4ccd2fcb3b9fd77b4
Alex Williams <alex.h.willia@gmail.com>
2015-10-14 09:41:59 -0700

    fix tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -14,7 +14,6 @@ function test_algorithm(fh)
 	x = [0.15512102, 0.69328985] # approx solution from scipy
 	@test norm(fh(A,b)-x) < 1e-5
 
-
 	## A second test case
 	A2 = [ -0.24  -0.82   1.35   0.36   0.35
 	       -0.53  -0.20  -0.76   0.98  -0.54
@@ -23,15 +22,19 @@ function test_algorithm(fh)
 	        0.48  -2.25   0.38   0.06  -1.29 ]
 	b2 = [-1.6,  0.19,  0.17,  0.31, -1.27]
 	x2 = [2.2010416, 1.19009924, 0.0, 1.55001345, 0.0]
-	@test norm(nnls(A2,b2)-x2) < 1e-5
+	@test norm(fh(A2,b2)-x2) < 1e-5
 
 	## Test a bunch of random cases
-	for i = 1:10
+	for i = 1:100
 		m,n = rand(1:10),rand(1:10)
 		A3 = randn(m,n)
 		b3 = randn(m)
 		x3,resid = pyopt.nnls(A3,b3)
-		@test norm(nnls(A3,b3)-x3) < 1e-5
+		if resid > 1e-5
+	        @test norm(fh(A3,b3)-x3) < 1e-5
+	    else
+	    	@test norm(A3*fh(A3,b3)-b3) < 1e-5
+	    end
 	end
 end
 
